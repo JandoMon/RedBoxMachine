5fbdaab09ba76ea501c97a16d797b43967421c53
JandoMon <paulalemontano@gmail.com>
2024-07-22 16:26:24 -0500

    implemented valid user txt and basic functionality

--- ReFormat RedBoxInventory/RedBoxInventorySystem/Main.java
+++ ReFormat RedBoxInventory/RedBoxInventorySystem/Main.java
@@ -1,20 +1,9 @@
 package RedBoxInventorySystem;
 
-import RedBoxInventorySystem.RedBoxFunctions.Movie.Transactions;
-import RedBoxInventorySystem.RedBoxFunctions.StorageOperations.InventoryOperations;
-
 public class Main {
 
     public static void main(String[] args) {
 
         kiosk.kioskActive();
-        /*
-         * String[] commandParts = kiosk.breakUpCommand("LESS COPIES \"Forest Gump\"");
-         * 
-         * System.out.println(commandParts.length);
-         * for (int i = 0; i < commandParts.length; i++) {
-         * System.out.println(commandParts[i]);
-         * }
-         */
     }
 }
